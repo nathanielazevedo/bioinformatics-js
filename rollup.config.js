a8b71dca91d20554e29bab4235de380fa1b2d497
nateazevedo <nathanielazevedo@hotmail.com>
2025-09-14 17:00:06 -0400

    add rollup config

---
+++ rollup.config.js
@@ -0,0 +1,29 @@
+import typescript from '@rollup/plugin-typescript';
+import resolve from '@rollup/plugin-node-resolve';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+
+export default {
+  input: 'src/index.ts',
+  output: [
+    {
+      file: 'dist/index.js',
+      format: 'cjs',
+      sourcemap: true,
+    },
+    {
+      file: 'dist/index.esm.js',
+      format: 'esm',
+      sourcemap: true,
+    },
+  ],
+  plugins: [
+    peerDepsExternal(),
+    resolve(),
+    typescript({
+      tsconfig: './tsconfig.json',
+      declaration: false,
+      declarationMap: false,
+    }),
+  ],
+  external: ['react', 'react-dom', 'd3'],
+};
